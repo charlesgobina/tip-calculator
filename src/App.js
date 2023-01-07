5b44e5c805627fa01390da33e1e593ce80d383f8
charlesgobina <charles.gobina@gmail.com>
2023-01-07 21:37:11 +0100

    Fix linter errors and add logo component

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Logo from './components/Logo/Logo';
+
+const App = () => (
+  <Logo />
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>hello world</h1>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
-
