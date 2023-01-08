d99205cdc59031d9517304ae3ac21107679e22a5
charlesgobina <charles.gobina@gmail.com>
2023-01-08 01:09:06 +0100

    Add Bill, Display, Tip, People and Wrapper components

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,7 @@
 import { ReactComponent as TipLogo } from '../../assets/images/logo.svg';
 
 const Logo = () => (
-  <div style={{ textAlign: 'center', padding: '50px 0' }}>
+  <div style={{ textAlign: 'center', padding: '40px 0' }}>
     <TipLogo />
   </div>
 );
